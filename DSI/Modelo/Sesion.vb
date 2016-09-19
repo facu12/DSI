94863eb66099e3fc651e19b4cc21cb3d5941370a
Nicolas Soria <nico_axe8@hotmail.com>
2016-09-19 00:48:23 -0300

    Hice un singleton, para que lo podamos llamar de cualquier lado

--- DSI/Modelo/Sesion.vb
+++ DSI/Modelo/Sesion.vb
@@ -1,6 +1,19 @@
 ﻿Public Class Sesion
     Property usuario As Usuario
 
+    Private Shared instance As Sesion 'Unica instancia de la clase
+
+    Private Sub New(usuario As Usuario)
+        usuario = usuario
+    End Sub
+
+    Public Shared Function getSesion(user As Usuario) As Sesion
+        If IsNothing(instance) Then
+            instance = New Sesion(user)
+        End If
+        Return instance
+    End Function
+
     Public Function buscarInvestigadorLogueado() As Usuario
         Return Me.usuario
     End Function
